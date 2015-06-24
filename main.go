b9ffd25bdb0819d4371fd9dc1f53d6d0bfe853ed
nylar <me@nylar.io>
2015-06-24 16:46:06 -0400

    Setup frontend, uses Prepros for building source

--- main.go
+++ main.go
@@ -1,6 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/nylar/triage/api"
+)
 
 // Version
 const (
@@ -13,4 +20,19 @@ var version = fmt.Sprintf("%d.%d.%d", MAJOR, MINOR, PATCH)
 
 func main() {
 	fmt.Printf("Triage, v%s\n", version)
+
+	// Initialse a new router and turn on missing trailing slash redirect
+	m := mux.NewRouter()
+	m.StrictSlash(true)
+
+	// Setup API routes
+	api.Routes(m)
+
+	// Setup a route for the public facing site
+	m.PathPrefix("/").Handler(http.FileServer(http.Dir("./public/")))
+
+	http.Handle("/", m)
+
+	log.Println("Serving on port :3030.")
+	http.ListenAndServe(":3030", nil)
 }
