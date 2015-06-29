7749eb30292c0869041675ef6a659dc54691834c
nylar <me@nylar.io>
2015-06-29 02:16:17 -0400

    Render tickets API endpoint using LiveScript

--- private/livescript/main.ls
+++ private/livescript/main.ls
@@ -1,1 +1,19 @@
-console.log 'Triage'
+Ticket = {list: ->
+	m.request {
+		method: 'GET'
+		url: '/api/tickets/'
+	}
+}
+
+App = {
+	controller: ->
+		tickets = Ticket.list!
+		{
+			tickets: tickets
+		}
+	view: (ctrl) ->
+		m 'div', [(ctrl.tickets!.map ((ticket) -> m 'p', ticket.message))]
+}
+
+m.mount (document.getElementById 'tickets'), App
+
--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,1 @@
-(function(){console.log("Triage")}).call(this);
+(function(){var t,e;t={list:function(){return m.request({method:"GET",url:"/api/tickets/"})}},e={controller:function(){var e;return e=t.list(),{tickets:e}},view:function(t){return m("div",[t.tickets().map(function(t){return m("p",t.message)})])}},m.mount(document.getElementById("tickets"),e)}).call(this);
